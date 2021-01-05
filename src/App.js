44d374826c0d52a5156905e5f073e47ffa31064b
Josh <jrogan@wikia-inc.com>
2021-01-05 18:25:27 -0500

    death pool

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ const Wrapper = styled.div`
 function App() {
   return (
     <Wrapper>
-      <iframe width="100%" height="100%" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vRzLhwx8OsdXovrGZhd8TNPHMx67yUvBTb4aUSz88JTimVsAhuNJ-GnoRFpMGc-k1U4Ec4rDmQgWLy3/pubhtml?widget=true&amp;headers=false"></iframe>
+      <iframe width="100%" height="100%" src="https://docs.google.com/spreadsheets/d/15s8PgJ8uZNcUsUEvM2gO2Si3fCJ4spwsAqh6f0xxJGM/edit?ouid=110939666120885358976&usp=sheets_home&ths=true"></iframe>
     </Wrapper>
   );
 }
