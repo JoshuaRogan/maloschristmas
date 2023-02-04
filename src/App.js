cc2923e369a2b0487a6edc0f37ddfd3fcad99147
Josh Rogan <jrogan@wikia-inc.com>
2023-02-03 19:59:33 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ const Wrapper = styled.div`
 function App() {
   return (
     <Wrapper>
-      <iframe width="100%" height="100%" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vRzLhwx8OsdXovrGZhd8TNPHMx67yUvBTb4aUSz88JTimVsAhuNJ-GnoRFpMGc-k1U4Ec4rDmQgWLy3/pubhtml?widget=true&amp;headers=false"></iframe>
+      <iframe title="Spreadsheet" width="100%" height="100%" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vRzLhwx8OsdXovrGZhd8TNPHMx67yUvBTb4aUSz88JTimVsAhuNJ-GnoRFpMGc-k1U4Ec4rDmQgWLy3/pubhtml?widget=true&amp;headers=false"></iframe>
     </Wrapper>
   );
 }
