dded50d524b8e0e91100278bccf39f3fe9fd4615
Josh <jrogan@wikia-inc.com>
2025-08-18 15:29:18 -0400

    Add images gallery

--- src/index.js
+++ src/index.js
@@ -3,10 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import WinnerImagesGallery from './components/WinnerImagesGallery';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootEl = document.getElementById('root');
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+ReactDOM.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/winner-images" element={<WinnerImagesGallery />} />
+    </Routes>
+  </BrowserRouter>,
+  rootEl,
+);
+
 serviceWorker.unregister();
